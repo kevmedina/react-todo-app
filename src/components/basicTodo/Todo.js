ae96363af46da0377d7a1f10692349dcef1486ec
Evgeny Galitskiy <gha665@gmail.com>
2020-05-31 15:11:21 -0400

    added delete todo

--- src/components/basicTodo/Todo.js
+++ src/components/basicTodo/Todo.js
@@ -1,13 +1,17 @@
 import React from "react";
 
-const Todo = ({ todo }) => {
-  console.log("Todo: ", todo);
+const Todo = ({ todo, deleteTodo }) => {
+  // console.log("Todo: ", todo);
+  const handleRemove = () => {
+    deleteTodo(todo.id);
+  };
+
   return (
     <div className="todo">
-        {todo.task}
-        <span>
-          <i className="fa fa-trash"></i>
-        </span>
+      {todo.task}
+      <span>
+        <i className="fa fa-trash" onClick={handleRemove}></i>
+      </span>
     </div>
   );
 };
--- src/components/basicTodo/TodoList.js
+++ src/components/basicTodo/TodoList.js
@@ -1,11 +1,11 @@
 import React from "react";
 import Todo from "./Todo";
 
-const TodoList = ({ todos }) => {
+const TodoList = ({ todos, deleteTodo }) => {
   return (
     <div className="todo-list">
       {todos.map((todo) => {
-        return <Todo key={todo.id} todo={todo} />;
+        return <Todo key={todo.id} todo={todo} deleteTodo={deleteTodo} />;
       })}
     </div>
   );
